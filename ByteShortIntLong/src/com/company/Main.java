8a4857f1e8879831ad76d267fc022a7683e78e33
Prem Roshan Nair <premroshan@gmail.com>
2020-07-16 00:26:18 +0530

    second project

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int myIntValue = 10000;
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum value = " + myMinIntValue);
+        System.out.println("Integer Maximum value = " + myMaxIntValue);
+
     }
 }
