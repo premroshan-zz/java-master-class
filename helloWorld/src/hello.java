3d5a94b324fe113a0d6e8134392199ff50136af5
Prem Roshan Nair <premroshan@gmail.com>
2020-07-15 23:57:21 +0530

    new update

--- helloWorld/src/hello.java
+++ helloWorld/src/hello.java
@@ -1,7 +1,14 @@
 public class hello {
     public static void main(String[] args){
+        int myFirstNumber = ( 5 * 2 ) + 15;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
         System.out.println("Hello Prem");
-        int myFirstNumber = 5;
         System.out.println(myFirstNumber);
+        System.out.println("Mythirdnumber = " + myThirdNumber);
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
