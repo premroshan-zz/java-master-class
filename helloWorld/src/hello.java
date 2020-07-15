d2285f3172e2025e8df17e174b4405a338dd6fac
Prem Roshan Nair <premroshan@gmail.com>
2020-07-15 23:37:03 +0530

    first commit

---
+++ helloWorld/src/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args){
+        System.out.println("Hello Prem");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
