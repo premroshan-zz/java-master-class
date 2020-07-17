059790edc0aeca8c517d4a98c385f270e77d3a7c
Prem Roshan Nair <premroshan@gmail.com>
2020-07-17 17:00:28 +0530

    char First commit commit for chars and boolean

--- charAndBoolean/src/com/prem/Main.java
+++ charAndBoolean/src/com/prem/Main.java
@@ -5,5 +5,17 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         char myChar = 'P';
+        char myUnicodeChar = '\u0916';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+        System.out.println(isCustomerOverTwentyOne );
     }
 }
