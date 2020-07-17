878870648293a83a67a3d4fe344453a92cc00264
Prem Roshan Nair <premroshan@gmail.com>
2020-07-17 12:08:54 +0530

    primitive type challenge

---
+++ primitiveTypeChallenge/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        byte myByteValue = 50;
+        short myShortValue = 10500;
+        int myIntValue = 50500;
+        long myLongValue = 50000 + 10 * ( myByteValue + myShortValue + myIntValue );
+
+        System.out.println("The total is = " + myLongValue);
+    }
+}
