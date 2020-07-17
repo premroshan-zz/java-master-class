d01d762ace9aef958a6712f9bc2dcd6a5cec95fd
Prem Roshan Nair <premroshan@gmail.com>
2020-07-17 12:03:44 +0530

    trying casting

--- ByteShortIntLong/src/com/company/Main.java
+++ ByteShortIntLong/src/com/company/Main.java
@@ -11,10 +11,35 @@ public class Main {
         System.out.println("Integer Minimum value = " + myMinIntValue);
         System.out.println("Integer Maximum value = " + myMaxIntValue);
 
+        //byte data type
         byte myMinByteValue = Byte.MIN_VALUE;
         byte myMaxByteValue = Byte.MAX_VALUE;
 
         System.out.println("Minimum Byte Value = " + myMinByteValue);
         System.out.println("Maximum Byte Value = " + myMaxByteValue);
+
+        //short
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Minimum Short Value = " + myMinShortValue);
+        System.out.println("Maximum Short Value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+
+        //long
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Minimum Long Value = " + myMinLongValue);
+        System.out.println("Maximum Long Value = " + myMaxLongValue);
+
+        long bigLongIntValue = 21474836471234L;
+
+        System.out.println(bigLongIntValue);
+
+        int myNewIntValue = myMinIntValue / 2;
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+        short myNewShortValue = (short) ( myMinShortValue / 2);
     }
 }
