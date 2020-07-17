9af9876218c06025471ad0b4bdc930f0ffa6ce7f
Prem Roshan Nair <premroshan@gmail.com>
2020-07-17 23:28:43 +0530

    operators

--- operators/src/com/prem/Main.java
+++ operators/src/com/prem/Main.java
@@ -20,5 +20,24 @@ public class Main {
 
         result = result % 3;
         System.out.println("4 mod 3 = " + result);
+
+        //result = result + 1
+        result++; // 1 + 1
+        System.out.println("One plus one = " + result);
+
+        result--;
+        System.out.println("Two minus one = " + result);
+
+        result += 2; // result = result + 2
+        System.out.println("One Plus two = " + result);
+
+        result *= 10;
+        System.out.println("3 * 10 = " + result);
+
+        result /= 3;
+        System.out.println("30 / 3 = " + result);
+
+        result -= 2;
+        System.out.println("10 -2 =" + result);
     }
 }
