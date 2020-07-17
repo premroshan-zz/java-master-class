590666551f8c53722d3f2775323cae97e4521f70
Prem Roshan Nair <premroshan@gmail.com>
2020-07-17 12:25:46 +0530

    Initial commit

---
+++ floatAndDouble/src/com/prem/Main.java
@@ -0,0 +1,19 @@
+package com.prem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("Minimum and Maximum float values are: " + myMinFloatValue + " and " +
+                myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Minimum and Maximum double values are: " + myMinDoubleValue + " and " +
+                    myMaxDoubleValue);
+    }
+}
