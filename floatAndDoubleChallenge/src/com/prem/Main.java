93527284baa9338acea5b38398ab6acd30830ac7
Prem Roshan Nair <premroshan@gmail.com>
2020-07-17 13:28:34 +0530

    float and double challenge

---
+++ floatAndDoubleChallenge/src/com/prem/Main.java
@@ -0,0 +1,11 @@
+package com.prem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int pounds = 150;
+        double kilos = (double) (pounds * 0.45359237d);
+        System.out.println(pounds + " number of pounds in Kilograms = " + kilos);
+    }
+}
