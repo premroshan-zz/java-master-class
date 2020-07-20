3a357e1b91763ea9a185c48a87d2414dadadc3a7
Prem Roshan Nair <premroshan@gmail.com>
2020-07-20 14:57:08 +0530

    changes to method

--- methods/src/com/prem/Main.java
+++ methods/src/com/prem/Main.java
@@ -18,11 +18,35 @@ public class Main {
         bonus = 200;
 
         if(gameOver){
-            System.out.println("Your final score is =" + calculateScore(score,levelComplete,bonus));
+            int highScore = calculateScore(score,levelComplete,bonus);
+            System.out.println("Your final score is =" + highScore );
         }
+
+        int highScorePosition = calculateHighScorePosition(200);
+        displayHighScorePosition("Tim",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(200);
+        displayHighScorePosition("Tim",highScorePosition);
+
     }
 
     public static int calculateScore(int score, int levelComplete, int bonus){
         return ( score + (levelComplete * bonus ) );
+    }
+
+    public static void displayHighScorePosition(String playerName, int position){
+        System.out.println("Play " + playerName + " managed to get into position "+ position);
+    }
+
+    public static int calculateHighScorePosition(int score){
+        if(score > 1000){
+            return 1;
+        }else if (score > 500 && score < 1000){
+            return 2;
+        }else if (score > 100 && score < 500){
+            return 3;
+        }else{
+            return 4;
+        }
     }
 }
