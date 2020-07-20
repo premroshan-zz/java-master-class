c6ec3e1b5845a8f824537e5155533db5c924a5c9
Prem Roshan Nair <premroshan@gmail.com>
2020-07-20 13:13:32 +0530

    statements

---
+++ statementWhitespaceAndIndenting/src/com/prem/Main.java
@@ -0,0 +1,16 @@
+package com.prem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("There is another" +
+                " still more" +
+                " yet more");
+    }
+}
