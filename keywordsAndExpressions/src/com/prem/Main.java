09d6da46546b9ae51296f5a56e92554094439b01
Prem Roshan Nair <premroshan@gmail.com>
2020-07-20 11:15:46 +0530

    keywords and expressions

---
+++ keywordsAndExpressions/src/com/prem/Main.java
@@ -0,0 +1,14 @@
+package com.prem;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double kilometers = ( 100 * 1.609344 );
+
+        int highScore = 50;
+        if(highScore == 50){
+            System.out.println("This is an expression");
+        }
+    }
+}
