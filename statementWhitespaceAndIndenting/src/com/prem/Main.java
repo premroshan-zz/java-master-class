aa407095e71f791eab418b33569e058a146fb88d
Prem Roshan Nair <premroshan@gmail.com>
2020-07-20 13:19:31 +0530

    updates

--- statementWhitespaceAndIndenting/src/com/prem/Main.java
+++ statementWhitespaceAndIndenting/src/com/prem/Main.java
@@ -3,7 +3,7 @@ package com.prem;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         int myVariable = 50;
         myVariable++;
         myVariable--;
